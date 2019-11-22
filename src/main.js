f7678088e5cf1f994e322a220a22e47741f3a186
Saul Ordonez <sauleordonez@gmail.com>
2019-11-22 14:56:37 -0800

    get some functionality from api call

--- src/main.js
+++ src/main.js
@@ -15,35 +15,36 @@ import './styles.css';
 })();
 
 const getElements = function(response) {
-  $('#firstName').text(response.data.);
+  console.log(response.data[0].profile.slug);
+  $('#firstName').text(response.data[0].profile.first_name);
 };
 // $(document).ready(function() {
-//   $('#submitForm').submit(function(event) {
-//     event.preventDefault();
+  //   $('#submitForm').submit(function(event) {
+    //     event.preventDefault();
 
     // const doctorName = $('#nameInput').val();
 
     // const getElements = function(response) {
-    //   let length = response.data.length;
-    //   $('#outputInfo').text(" ");
-    //
-    //   //https://www.w3schools.com/jsref/met_document_createelement.asp
-    //   //used for creating node
-    //
-    //   for(let i = 0; i < length; i++){
-    //     let node = document.createElement("LI");
-    //     let textnode = document.createTextNode(`Name: ${response.data[i].profile.first_name}`);
-    //     node.appendChild(textnode);
-    //     document.getElementById("#outputInfo").appendChild(node);
-    //   }
-    //
-    // };
+      //   let length = response.data.length;
+      //   $('#outputInfo').text(" ");
+      //
+      //   //https://www.w3schools.com/jsref/met_document_createelement.asp
+      //   //used for creating node
+      //
+      //   for(let i = 0; i < length; i++){
+        //     let node = document.createElement("LI");
+        //     let textnode = document.createTextNode(`Name: ${response.data[i].profile.first_name}`);
+        //     node.appendChild(textnode);
+        //     document.getElementById("#outputInfo").appendChild(node);
+        //   }
+        //
+        // };
 
-    (async () => {
-      let doctorNameSearch = new DoctorNameSearch();
-      let response = await doctorNameSearch.getDoctorNameSearch(doctorName);
-      getElements(response);
-    })();
+        (async () => {
+          let betterDoctorAPI = new BetterDoctorAPI();
+          let response = await betterDoctorAPI.getBetterDoctorAPI();
+          getElements(response);
+        })();
 
-//   });
-// });
+        //   });
+        // });
