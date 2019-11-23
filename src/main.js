7d2e4b4fe7695f5a9491ef394695627bc32478ef
Saul Ordonez <sauleordonez@gmail.com>
2019-11-22 16:36:37 -0800

    restructure using createElements method to loop through arrays

--- src/main.js
+++ src/main.js
@@ -4,60 +4,39 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// API Framework
+$(document).ready(function() {
+  $('#submitForm').submit(function(event) {
+    event.preventDefault();
 
+    const doctorName = $('#nameInput').val();
 
-//this API call works fine
+    (async () => {
+      let betterDoctorAPI = new BetterDoctorAPI();
+      let response = await betterDoctorAPI.getBetterDoctorAPI(doctorName);
+      getElements(response);
+    })();
 
-// (async () => {
-  //   let betterDoctorAPI = new BetterDoctorAPI();
-  //   const response = await betterDoctorAPI.getBetterDoctorAPI(doctorName);
-  //   getElements(response);
-  // })();
-  //
-  // const getElements = function(response) {
-    //   $('#firstName').text(response.data[0].profile.first_name);
-    // };
+    const getElements = function(response) {
+      let dataLength = response.data.length;
+      $("#outputInfo").text(" ");
 
-    $(document).ready(function() {
-      $('#submitForm').submit(function(event) {
-        event.preventDefault();
+      for(let i = 0;i < dataLength; i++){
+        let node = document.createElement("LI");
+        let textnode = document.createTextNode(`Name: ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} Phone: ${response.data[i].practices[0].phones[0].number} Address: ${response.data[i].practices[0].visit_address.state}  City: ${response.data[i].practices[0].visit_address.city} Street: ${response.data[i].practices[0].visit_address.street} Zip: ${response.data[i].practices[0].visit_address.zip} Accepting new Patents: ${response.data[i].practices[0].accepts_new_patients}`);
+        node.appendChild(textnode);
+        document.getElementById("outputInfo").appendChild(node);
+      }
+    };
 
-        const doctorName = $('#nameInput').val();
+    // const getElements = function(response) {
+      //   $('#firstName').text(response.data[0].profile.first_name);
+      //   $('#lastName').text(response.data[0].profile.last_name);
+      //   $('#title').text(response.data[0].profile.title);
+      //   $('#address').text(response.data[0].practices[0].visit_address.street + " " +  response.data[0].practices[0].visit_address.city + " " + response.data[0].practices[0].visit_address.state + ", " + response.data[0].practices[0].visit_address.zip);
+      //   $('#phoneNumber').text(response.data[0].practices[0].phones[0].number);
+      //   $('#website').text(response.data[0].profile.first_name);
+      //   $('#available').text(response.data[0].practices[0].accepts_new_patients);
+      // };
 
-        (async () => {
-          let betterDoctorAPI = new BetterDoctorAPI();
-          let response = await betterDoctorAPI.getBetterDoctorAPI(doctorName);
-          getElements(response);
-        })();
-
-        const getElements = function(response) {
-          $('#firstName').text(response.data[0].profile.first_name);
-          $('#lastName').text(response.data[0].profile.last_name);
-          $('#title').text(response.data[0].profile.title);
-          $('#address').text(response.data[0].practices[0].visit_address.street + response.data[0].practices[0].visit_address.city + response.data[0].practices[0].visit_address.state + ", " + response.data[0].practices[0].visit_address.zip);
-          $('#phoneNumber').text(response.data[0].practices[0].phones[0].number);
-          $('#website').text(response.data[0].profile.first_name);
-          $('#available').text(response.data[0].practices[0].accepts_new_patients);
-
-
-
-
-          //   let length = response.data.length;
-          //   $('#outputInfo').text(" ");
-          //
-          //   //https://www.w3schools.com/jsref/met_document_createelement.asp
-          //   //used for creating node
-          //
-          //   for(let i = 0; i < length; i++){
-            //     let node = document.createElement("LI");
-            //     let textnode = document.createTextNode(`Name: ${response.data[i].profile.first_name}`);
-            //     node.appendChild(textnode);
-            //     document.getElementById("#outputInfo").appendChild(node);
-            //   }
-            //
-          };
-
-
-        });
-      });
+    });
+  });
